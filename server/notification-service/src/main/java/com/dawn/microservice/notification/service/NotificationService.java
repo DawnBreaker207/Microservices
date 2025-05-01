16ba1fc600b8e5a4f10c36571ccecbb611c8afd3
DawnBreaker <tunganhngo207@gmail.com>
2025-05-02 00:34:29 +0700

    feat: update notification service

---
+++ server/notification-service/src/main/java/com/dawn/microservice/notification/service/NotificationService.java
@@ -0,0 +1,52 @@
+package com.dawn.microservice.notification.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+
+import com.dawn.microservice.notification.order.OrderPlacedEvent;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class NotificationService {
+
+    private final JavaMailSender javaMailSender;
+
+    @KafkaListener(topics = "order-placed")
+    public void listen(OrderPlacedEvent orderPlacedEvent) {
+	log.info("Got Message from order-placed topic {}", orderPlacedEvent);
+	MimeMessagePreparator messagePerparator = mimeMessage -> {
+	    MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+	    messageHelper.setFrom("springshop@email.com");
+	    messageHelper.setTo(orderPlacedEvent.getEmail().toString());
+	    messageHelper.setSubject(String.format("Your Order with OrderNumber %s is placed successfully",
+		    orderPlacedEvent.getOrderNumber()));
+	    messageHelper.setText(String.format("""
+	    	Hi
+
+	    	Your order with order number %s is now placed successfully
+
+	    	Best Regards
+
+	    	Spring Shop
+	    	""", null));
+
+	    orderPlacedEvent.getOrderNumber();
+	};
+
+	try {
+	    javaMailSender.send(messagePerparator);
+	    log.info("Order Notification email send!!");
+	} catch (MailException e) {
+	    log.error("Exception occurred when sending email", e);
+	    throw new RuntimeException("Exception occurred when sending mail to springshop@email.com", e);
+	}
+    }
+}
