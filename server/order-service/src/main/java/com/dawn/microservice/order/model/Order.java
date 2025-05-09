654634d0cd74db16daed505741755ffe72c60af1
DawnBreaker <tunganhngo207@gmail.com>
2025-05-10 03:34:33 +0700

    feat: update order service config

--- server/order-service/src/main/java/com/dawn/microservice/order/model/Order.java
+++ server/order-service/src/main/java/com/dawn/microservice/order/model/Order.java
@@ -2,22 +2,18 @@ package com.dawn.microservice.order.model;
 
 import java.math.BigDecimal;
 
-
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Table(name = "t_orders")
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Order {
