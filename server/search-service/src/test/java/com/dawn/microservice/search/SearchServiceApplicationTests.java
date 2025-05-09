076145bfaab6c9206b6927e9bc74005591a451fc
DawnBreaker <tunganhngo207@gmail.com>
2025-05-10 03:40:15 +0700

    feat: update search service

---
+++ server/search-service/src/test/java/com/dawn/microservice/search/SearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawn.microservice.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
