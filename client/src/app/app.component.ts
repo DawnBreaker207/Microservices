670b2281794010f7a5da2bc035bd05258049a5b4
DawnBreaker <tunganhngo207@gmail.com>
2025-05-16 16:17:34 +0700

    feat: update app config

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,23 +1,24 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./shares/header/header.component";
 import { OidcSecurityService } from 'angular-auth-oidc-client';
+import { ResponsiveHelperComponent } from './shared/components/responsive-helper/responsive-helper.component';
+import { ThemeService } from './core/services/theme.service';
+import { NgxSonnerToaster } from 'ngx-sonner';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, ResponsiveHelperComponent, NgxSonnerToaster],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
   title = 'microservices-shop-frontend';
-private readonly oidSecurityService = inject(OidcSecurityService);
+  constructor(private readonly oidSecurityService: OidcSecurityService, public themeService: ThemeService) {}
 
   ngOnInit(): void {
-    this.oidSecurityService.checkAuth().subscribe(({isAuthenticated}) =>{
+    this.oidSecurityService.checkAuth().subscribe(({ isAuthenticated }) => {
       console.log('app authenticated', isAuthenticated);
-
-    })
+    });
   }
 }
--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,12 +1,18 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { authInterceptor } from './interceptor/auth.interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideAuth } from 'angular-auth-oidc-client';
-import { authConfig } from './config/auth.config';
+import { routes } from './app-routing.module';
+import { authConfig } from './core/config/auth.config';
+import { authInterceptor } from './core/interceptor/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withInterceptors([authInterceptor])), provideAuth(authConfig)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideAuth(authConfig),
+    provideAnimationsAsync(),
+  ],
 };
