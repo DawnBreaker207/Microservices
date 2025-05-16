8bad46e4062be23ccc6f4b876f83fb46c4dacdfd
DawnBreaker <tunganhngo207@gmail.com>
2025-05-16 16:26:32 +0700

    feat: update order

---
+++ client/src/app/modules/admin/components/order/create/create.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-order',
+  standalone: true,
+  imports: [],
+  templateUrl: './create.component.html',
+  styleUrl: './create.component.css',
+})
+export class CreateOrderComponent {}
