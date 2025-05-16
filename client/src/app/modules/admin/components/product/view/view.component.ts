35c20a6b9763c2074e122c8bde9b2a6b945431b4
DawnBreaker <tunganhngo207@gmail.com>
2025-05-16 16:26:51 +0700

    feat: update product admin

---
+++ client/src/app/modules/admin/components/product/view/view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-product',
+  standalone: true,
+  imports: [],
+  templateUrl: './view.component.html',
+  styleUrl: './view.component.css',
+})
+export class ViewProductComponent {}
