d85957ff227bc8bdb1d73ad2ca6b2d0b87a7c2c7
DawnBreaker <tunganhngo207@gmail.com>
2025-05-18 13:24:56 +0700

    feat: update shared modules

---
+++ client/src/app/shared/models/chart-options.ts
@@ -0,0 +1,40 @@
+import {
+  ApexAnnotations,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexDataLabels,
+  ApexFill,
+  ApexGrid,
+  ApexLegend,
+  ApexMarkers,
+  ApexPlotOptions,
+  ApexResponsive,
+  ApexStates,
+  ApexStroke,
+  ApexTheme,
+  ApexTitleSubtitle,
+  ApexTooltip,
+  ApexXAxis,
+  ApexYAxis,
+} from 'ng-apexcharts';
+
+export interface ChartOptions {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  legend: ApexLegend;
+  dataLabels: ApexDataLabels;
+  fill: ApexFill;
+  stroke: ApexStroke;
+  xaxis: ApexXAxis;
+  yaxis: ApexYAxis | ApexYAxis[];
+  states: ApexStates;
+  tooltip: ApexTooltip;
+  colors: string[];
+  grid: ApexGrid;
+  title: ApexTitleSubtitle;
+  theme: ApexTheme;
+  annotations: ApexAnnotations;
+  responsive: ApexResponsive[];
+  plotOptions: ApexPlotOptions;
+  markers: ApexMarkers;
+}
